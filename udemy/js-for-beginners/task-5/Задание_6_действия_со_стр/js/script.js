1730b6d184d13b03720709c8e8ff47ffe5f1381e
t.kotova <patrikklava@gmail.com>
2019-05-24 15:54:43 +0300

    Задание 6: Задание на отработку действий со страницей

--- udemy/js-for-beginners/task-5/Задание_6_действия_со_стр/js/script.js
+++ udemy/js-for-beginners/task-5/Задание_6_действия_со_стр/js/script.js
@@ -1,12 +1,25 @@
 'use strict';
 
 let menu = document.body.querySelector('.menu');
-
 let menuItem = document.querySelectorAll('.menu-item');
-
 menu.insertBefore(menuItem[2], menuItem[1]);
 
 let li = document.createElement('li');
 li.className = 'menu-item';
 li.textContent = 'Пятый пункт';
 menu.appendChild(li);
+
+let body = document.getElementsByTagName('body');
+body[0].style.backgroundImage = 'url("img/apple_true.jpg")';
+
+let title = document.getElementById('title');
+title.innerText = 'Мы продаем только подлинную технику Apple';
+
+let columnSecond = document.body.getElementsByClassName('column')[1];
+let blockAdv = document.getElementsByClassName('adv')[0];
+
+columnSecond.removeChild(blockAdv);
+
+let questionToUser = prompt('Как вы относитесь к технике Apple?');
+let saveAnswer = document.getElementsByClassName('prompt')[0];
+saveAnswer.textContent = questionToUser;
