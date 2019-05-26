5d9bb99200ee94db76cc0271933885805aba0fc1
t.kotova <patrikklava@gmail.com>
2019-05-27 00:00:11 +0300

    Задание 7: Практика. Пишем приложение. Часть 4

--- udemy/js-for-beginners/task-7/src/js/main.js
+++ udemy/js-for-beginners/task-7/src/js/main.js
@@ -17,3 +17,15 @@ let btnOptionalExp = document.getElementsByTagName('button')[1];
 let btnCountBudget = document.getElementsByTagName('button')[2];
 
 let inputOptionalExpItem = document.querySelectorAll('.optionalexpenses-item');
+
+let inputChooseIncome = document.querySelector('.choose-income');
+
+let checkbSavings = document.querySelector('#savings');
+
+let inputChooseSum = document.querySelector('.choose-sum');
+
+let inputChoosePercent = document.querySelector('.choose-percent');
+
+let inputYearVal = document.querySelector('.year-value');
+inputMonthVal = document.querySelector('.month-value');
+inputDayVal = document.querySelector('.day-value');
