ba47bdf8cf0825563ce8ff8ea0b1310537a81ea0
t.kotova <patrikklava@gmail.com>
2019-06-11 21:10:31 +0300

    Задание 12.2 сделано. class Option работает

--- udemy/js-for-beginners/tasks-9,10,11/task-12.2/script.js
+++ udemy/js-for-beginners/tasks-9,10,11/task-12.2/script.js
@@ -9,7 +9,21 @@ class Options {
     this.textAlign = textAlign;
   }
 
-  createDiv {
-    
+  createDiv() {
+    let newDiv = document.createElement('div');
+    let newSomeText = document.createTextNode('Hello, world! this some text=)');
+    newDiv.appendChild(newSomeText);
+    document.body.appendChild(newDiv);
+    let styleDiv = `
+    height: ${this.height}px; 
+    width: ${this.width}px; 
+    background-color: ${this.bg}; 
+    font-size: ${this.fontSize}px; 
+    text-align: ${this.textAlign}`;
+    newDiv.style.cssText = styleDiv;
   }
 }
+
+const someNewDiv = new Options(700, 550, 'plum', 14, 'center');
+
+someNewDiv.createDiv();
