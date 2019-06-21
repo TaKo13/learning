9b0d2786a68eae9e4d6807dc42726a7b7cf78154
t.kotova <patrikklava@gmail.com>
2019-06-21 20:39:13 +0300

    Задание 15.Слайдер завершен

--- udemy/js-for-beginners/tasks_9-16/yoga/js/script.js
+++ udemy/js-for-beginners/tasks_9-16/yoga/js/script.js
@@ -228,15 +228,48 @@ window.addEventListener('DOMContentLoaded', function() {
   let slides = document.querySelectorAll('.slider-item');
   let prev = document.querySelector('.prev');
   let next = document.querySelector('.next');
-  let dotWrap = document.querySelector('.slider-dots');
+  let dotsWrap = document.querySelector('.slider-dots');
   let dots = document.querySelectorAll('.dot');
 
   showSlides(slideIndex);
   function showSlides(index) {
+    if (index > slides.length) {
+      slideIndex = 1;
+    }
+    if (index < 1) {
+      slideIndex = slides.length;
+    }
+
     slides.forEach(item => (item.style.display = 'none'));
     dots.forEach(item => item.classList.remove('dot-active'));
 
     slides[slideIndex - 1].style.display = 'block';
     dots[slideIndex - 1].classList.add('dot-active');
   }
+
+  function plusSlides(index) {
+    showSlides((slideIndex += index));
+  }
+  function currentSlide(index) {
+    showSlides((slideIndex = index));
+  }
+
+  prev.addEventListener('click', function() {
+    plusSlides(-1);
+  });
+
+  next.addEventListener('click', function() {
+    plusSlides(1);
+  });
+
+  dotsWrap.addEventListener('click', function(event) {
+    for (let i = 0; i < dots.length + 1; i++) {
+      if (
+        event.target.classList.contains('dot') &&
+        event.target == dots[i - 1]
+      ) {
+        currentSlide(i);
+      }
+    }
+  });
 });
