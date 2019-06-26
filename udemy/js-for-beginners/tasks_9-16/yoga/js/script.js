a90691f49f18e749501a9caaea72def81597675e
t.kotova <patrikklava@gmail.com>
2019-06-26 21:02:41 +0300

    Задание 16, написание калькулятора

--- udemy/js-for-beginners/tasks_9-16/yoga/js/script.js
+++ udemy/js-for-beginners/tasks_9-16/yoga/js/script.js
@@ -272,4 +272,47 @@ window.addEventListener('DOMContentLoaded', function() {
       }
     }
   });
+
+  //  Calc
+
+  let persons = document.querySelectorAll('.counter-block-input')[0];
+  let restDays = document.querySelectorAll('.counter-block-input')[1];
+  let place = document.getElementById('select');
+  let totalValue = document.getElementById('total');
+  let personsSum = 0;
+  let daysSum = 0;
+  let total = 0;
+
+  totalValue.innerHTML = 0;
+
+  persons.addEventListener('change', function() {
+    personsSum = +this.value;
+    total = (daysSum + personsSum) * 4000;
+
+    if (restDays.value == '') {
+      totalValue.innerHTML = 0;
+    } else {
+      totalValue.innerHTML = total;
+    }
+  });
+
+  restDays.addEventListener('change', function() {
+    daysSum = +this.value;
+    total = (daysSum + personsSum) * 4000;
+
+    if (persons.value == '') {
+      totalValue.innerHTML = 0;
+    } else {
+      totalValue.innerHTML = total;
+    }
+  });
+
+  place.addEventListener('change', function() {
+    if (restDays.value == '' || persons.value == '') {
+      totalValue.innerHTML = 0;
+    } else {
+      let currTotal = total;
+      totalValue.innerHTML = currTotal * this.options[this.selectedIndex].value;
+    }
+  });
 });
