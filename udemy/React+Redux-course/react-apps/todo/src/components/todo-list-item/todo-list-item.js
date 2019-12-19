d533316b8e21e2961a09d48af846d8c68de9f159
t.kotova <patrikklava@gmail.com>
2019-12-19 18:17:33 +0300

    36, 37 урок. Работат с состояниями

--- udemy/React+Redux-course/react-apps/todo/src/components/todo-list-item/todo-list-item.js
+++ udemy/React+Redux-course/react-apps/todo/src/components/todo-list-item/todo-list-item.js
@@ -2,27 +2,38 @@ import React, { Component } from 'react';
 import './todo-list-item.css';
 
 export default class ToDoListItem extends Component {
-  constructor() {
-    super();
-    this.onLabelClick = () => {
-      console.log(`Done: ${this.props.label}`);
-    };
-  }
+  state = {
+    done: false,
+    important: false
+  };
+
+  onLabelClick = () => {
+    this.setState({
+      done: true
+    });
+  };
+
+  onMarkImportant = () => {
+    this.setState({
+      important: true
+    });
+  };
 
   render() {
-    const { label, important = false } = this.props;
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+    const { label } = this.props;
+    const { done, important } = this.state;
+
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done';
+    }
+    if (important) {
+      classNames += ' important';
+    }
 
     return (
-      <span className="todo-list-item">
-        <span
-          className="todo-list-item-label"
-          style={style}
-          onClick={this.onLabelClick}
-        >
+      <span className={classNames}>
+        <span className="todo-list-item-label" onClick={this.onLabelClick}>
           {label}
         </span>
 
@@ -30,6 +41,7 @@ export default class ToDoListItem extends Component {
           type="button"
           className="btn btn-outline-success 
           btn-sm float-right"
+          onClick={this.onMarkImportant}
         >
           <i className="fa fa-exclamation" />
         </button>
